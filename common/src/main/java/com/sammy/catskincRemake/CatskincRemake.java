a74c3020031d4c384c2238fe74738a1bcad3db4a
SamSu <chixnuchit494@gmail.com>
2026-02-25 14:11:37 +0700

    Add initial project structure with Gradle setup and basic mod files for Fabric and Forge

---
+++ common/src/main/java/com/sammy/catskincRemake/CatskincRemake.java
@@ -0,0 +1,9 @@
+package com.sammy.catskincRemake;
+
+public final class CatskincRemake {
+    public static final String MOD_ID = "catskinc-remake";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ fabric/src/main/java/com/sammy/catskincRemake/fabric/CatskincRemakeFabric.java
@@ -0,0 +1,16 @@
+package com.sammy.catskincRemake.fabric;
+
+import com.sammy.catskincRemake.CatskincRemake;
+import net.fabricmc.api.ModInitializer;
+
+public final class CatskincRemakeFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        // Run our common setup.
+        CatskincRemake.init();
+    }
+}
